1bea3dd90a3c92725b49424b482a2f7730bc2556
minato86 <doronomori660@outlook.jp>
2020-01-01 22:51:19 +0900

    add: main.jsを追加、初期ウィンドウを実装

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const electron = require('electron');
+const app = electron.app;
+const browserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new browserWindow({
+        width: 700,
+        height: 500,
+        
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/views/index.html`)
+    mainWindow.setMenu(null)
+    //mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
