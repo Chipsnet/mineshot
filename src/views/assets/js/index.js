075050ceeb91ba95ed3d16d2e969b85675c5739a
minato86 <doronomori660@outlook.jp>
2020-01-03 21:18:47 +0900

    add: node_modules add gitignore

--- src/views/assets/js/index.js
+++ src/views/assets/js/index.js
@@ -1,12 +1,12 @@
 const { dialog, clipboard, nativeImage } = require('electron').remote
 const chokidar = require("chokidar");
 const $ = require('./assets/js/jquery-3.4.1.min.js');
-const appendLog = (log_data) => $('#log').append(log_data + "\n").scrollTop(9e9);
+
+const AppendLog = (log_data) => $('#log').append(log_data + "\n").scrollTop(9e9);
 
 let status = false;
 
-$(() => appendLog('準備完了'));
-
+$(() => AppendLog('準備完了'));
 
 const OpenDirectory = () => dialog.showOpenDialog(null, {
     properties: ['openDirectory'],
@@ -14,7 +14,7 @@ const OpenDirectory = () => dialog.showOpenDialog(null, {
     defaultPath: `${process.env.APPDATA}\\.minecraft\\screenshots`
 });
 
-const toggleStatus = () => {
+const ToggleStatus = () => {
     $('footer').css('background-color', this.status ? 'cornflowerblue' : 'darkgoldenrod');
     $('footer > p').text(this.status ? '待機中' : '監視中');
     this.status = !this.status;
@@ -22,20 +22,20 @@ const toggleStatus = () => {
 }
 
 const StartChokidar = (dir) => {
-    appendLog(`${dir} の監視を開始しました`);
+    AppendLog(`${dir} の監視を開始しました`);
     chokidar.watch(dir, {ignoreInitial: true}).on('add', path => {
-        appendLog(`新規ファイル ${path} を検知しました`);
+        AppendLog(`新規ファイル ${path} を検知しました`);
         setTimeout(() => {
             let img = nativeImage.createFromPath(path);
             clipboard.writeImage(img);
-            appendLog(`画像をクリップボードにコピーしました`);
+            AppendLog(`画像をクリップボードにコピーしました`);
         }, 1000);
     });
 }
 
 $('#start').click(() => {
     console.log('Button id start is pushed');
-    toggleStatus();
+    ToggleStatus();
 });
 
 $("#test").click(() => {
@@ -47,7 +47,7 @@ $("#test").click(() => {
         message: '監視対象のフォルダを選択します。',
         detail: 'デフォルトのフォルダから変更していない場合、そのままフォルダを選択します。'
     }).then(() => OpenDirectory().then(result => {
-        appendLog(`監視対象フォルダを ${result.filePaths[0]} に設定しました`);
+        AppendLog(`監視対象フォルダを ${result.filePaths[0]} に設定しました`);
         $('#path').val(result.filePaths);
         StartChokidar(result.filePaths[0]);
     }));
