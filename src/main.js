d2781d46a10f472f4b2c656cb47623be2608f2cd
hiragino_yuki <45731869+HiraginoYuki@users.noreply.github.com>
2020-01-03 18:32:27 +0900

    browserWindow to BrowserWindow

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 const chain = function c(o){return new Proxy(o,{get:(t,p)=>(...a)=>(t[p](...a),c(t))})};
-const { app, browserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron');
 
 let mainWindow = null;
 
 app.on('ready', () => {
-    chain(mainWindow = new browserWindow({
+    chain(mainWindow = new BrowserWindow({
         width: 700,
         height: 500,
         webPreferences: { nodeIntegration: true }
